804aa431965c034b5b30fbf1b0ba0babe0105014
rosamondkim <itzmems@naver.com>
2024-04-04 15:34:15 +0900

    :lipstick: UI: Header ui 완성
    header ui작업끝

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Header.module.css";
 import Logo from "../../assets/weniv-api-logo.svg";
-import World from "../../assets/icon-world.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const Header = () => {
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
   return (
     <>
       <header>
@@ -14,15 +15,34 @@ const Header = () => {
           </h1>
           <nav className={styles.header_nav}>
             <ul className={styles.nav_list}>
-              <li className={styles.active}>
-                <Link>eduAPI 소개</Link>
+              <li className={isActive("/landing") ? styles.active : ""}>
+                <Link to="/landing">eduAPI 소개</Link>
               </li>
-              <li>
-                <Link>CRUD API 명세서</Link>
+              <li className={isActive("/") ? styles.active : ""}>
+                <Link to="/">CRUD API 명세서</Link>
               </li>
             </ul>
-            <button className={styles.lang_btn}></button>
-            {/* 언어선택 */}
+            <div className={styles.lang_cont}>
+              <button className={styles.lang_btn}>
+                <span className="a11y-hidden"> 언어 선택</span>
+              </button>
+              <div className={styles.lang_modal}>
+                <ul className={styles.lang_list}>
+                  <li>
+                    KR
+                    <span className="a11y-hidden">한국어로 변경</span>
+                  </li>
+                  <li>
+                    EN
+                    <span className="a11y-hidden">Change to English</span>
+                  </li>
+                  <li>
+                    JP
+                    <span className="a11y-hidden">Change to Japanese</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </nav>
         </div>
       </header>
