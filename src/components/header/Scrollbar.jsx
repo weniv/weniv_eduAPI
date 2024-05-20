f71efba37ddc12848f712a67f542cc4b3acf9cbd
rosamondkim <itzmems@naver.com>
2024-05-20 17:23:44 +0900

    :sparkles: Add: 스크롤바 컴포넌트 추가
    스크롤바 기능 추가

--- src/components/header/Scrollbar.jsx
+++ src/components/header/Scrollbar.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { throttle } from "lodash";
 
 import styles from "./Scrollbar.module.css";
+import { useLocation } from "react-router-dom";
 
 export default function ScrollBar() {
   const [percentage, setPercentage] = useState(0);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = throttle(() => {
@@ -26,13 +28,16 @@ export default function ScrollBar() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  useEffect(() => {
+    setPercentage(0);
+  }, [location]);
 
   return (
     <div className={styles.scroll}>
       <div
-      // style={{
-      //   transform: `translateX(${-100 + percentage}%)`,
-      // }}
+        style={{
+          transform: `translateX(${-100 + percentage}%)`,
+        }}
       ></div>
     </div>
   );
