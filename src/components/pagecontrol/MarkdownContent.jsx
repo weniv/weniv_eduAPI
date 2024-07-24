33ad5e749c50a09bfac75641292e1796c0ddfbe1
rosamondkim <itzmems@naver.com>
2024-07-24 14:10:43 +0900

    :sparkles: Feat: 로딩 컴포넌트 추가

--- src/components/pagecontrol/MarkdownContent.jsx
+++ src/components/pagecontrol/MarkdownContent.jsx
@@ -4,9 +4,11 @@ import {
   convertMarkdownToHtml,
 } from "../../utils/convertMarkdowntoHtml";
 import "../../styles/markdown.css";
+import Loading from "../loading/Loading";
 
 const MarkdownContent = ({ markdownPath, onContentLoad }) => {
   const [htmlContent, setHtmlContent] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function loadMarkdownContent() {
@@ -14,9 +16,11 @@ const MarkdownContent = ({ markdownPath, onContentLoad }) => {
         const markdownText = await fetchMarkdownContent(markdownPath);
         const html = await convertMarkdownToHtml(markdownText);
         setHtmlContent(html);
+        setIsLoading(false);
         onContentLoad(); // Markdown이 로드된 후에 콜백 호출
       } catch (error) {
         console.error("데이터를 불러올 수 없습니다", error);
+        setIsLoading(false);
       }
     }
 
@@ -24,8 +28,13 @@ const MarkdownContent = ({ markdownPath, onContentLoad }) => {
 
     return () => {
       setHtmlContent(""); // 컴포넌트 언마운트 시 상태를 초기화
+      setIsLoading(true); // 컴포넌트 언마운트 시 로딩 상태를 초기화
     };
   }, [markdownPath, onContentLoad]);
+
+  if (isLoading) {
+    return <Loading color="primary" />; // 로딩 중일 때 SVGLoading 컴포넌트를 표시
+  }
 
   return (
     <main
--- src/components/pagecontrol/PageControl.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageControl = () => {
-  return <div>PageControl</div>;
-};
-
-export default PageControl;
