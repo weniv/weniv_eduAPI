97c37d2d1aaf8434e4374c2828510e1615edf8a2
rosamondkim <itzmems@naver.com>
2024-08-28 16:24:22 +0900

    :lipstick: Update: CSS 색상 변수명 일괄 변경

--- config-overrides.js
+++ config-overrides.js
@@ -8,5 +8,11 @@ module.exports = function override(config, env) {
     buffer: false,
   };
 
+  // 소스 맵 관련 경고 필터링 추가
+  if (!config.stats) {
+    config.stats = {};
+  }
+  config.stats.warningsFilter = [/Failed to parse source map/];
+
   return config;
 };
--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -14,7 +14,7 @@ const Loading = () => {
           <g>
             <path
               d="M23 110C35.7026 110 46 99.7026 46 87C46 74.2975 35.7026 64 23 64C10.2975 64 0 74.2975 0 87C0 99.7026 10.2975 110 23 110Z"
-              style={{ fill: "var(--W-primary)" }}
+              style={{ fill: "var(--primary)" }}
             />
             <animateTransform
               attributeName="transform"
@@ -28,7 +28,7 @@ const Loading = () => {
           <g>
             <path
               d="M97 110C109.703 110 120 99.7026 120 87C120 74.2975 109.703 64 97 64C84.2975 64 74 74.2975 74 87C74 99.7026 84.2975 110 97 110Z"
-              style={{ fill: "var(--W-primary)" }}
+              style={{ fill: "var(--primary)" }}
             />
             <animateTransform
               attributeName="transform"
@@ -42,7 +42,7 @@ const Loading = () => {
           <g>
             <path
               d="M171 110C183.703 110 194 99.7026 194 87C194 74.2975 183.703 64 171 64C158.297 64 148 74.2975 148 87C148 99.7026 158.297 110 171 110Z"
-              style={{ fill: "var(--W-primary)" }}
+              style={{ fill: "var(--primary)" }}
             />
             <animateTransform
               attributeName="transform"
