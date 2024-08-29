da006128f6fc1fd3f7d1e0a84abd8cb417b50ff6
rosamondkim <itzmems@naver.com>
2024-08-29 16:54:49 +0900

    :lipstick: Style: 반응형 코드 수정

--- src/components/main/Banner.jsx
+++ src/components/main/Banner.jsx
@@ -11,8 +11,7 @@ const Banner = () => {
             학습용 CRUD API, <span>eduAPI</span>
           </h2>
           <p className={styles.banner_intro}>
-            eduAPI와 함께 빠르고, 쉽게
-            비동기 통신 실습을 경험해 보세요!
+            eduAPI와 함께 빠르고, 쉽게 비동기 통신 실습을 경험해 보세요!
           </p>
           <Button children={`API 명세서 보러가기`} />
         </div>
