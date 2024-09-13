f34635b8df0bb4ca79fb60e6dfee6648a583ca0b
rosamondkim <itzmems@naver.com>
2024-09-13 13:56:52 +0900

    :lipstick: Style: 반응형 문제사항 수정

--- src/components/menu/AsideMobile.jsx
+++ src/components/menu/AsideMobile.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./AsideMobile.module.css";
 import Toggle from "../../assets/icon-down.svg";
 import Toc from "./Toc";
@@ -6,57 +6,43 @@ import SubBanner from "./SubBanner";
 
 const AsideMobile = () => {
   const [isMenuShow, setIsMenuShow] = useState(false);
-  const [isToggled, setIsToggled] = useState(false);
 
-  // const menuRef = useRef(null);
-  // // const lastBtn = useRef(null);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1024) {
+        setIsMenuShow(false);
+      }
+    };
 
-  // const handleFocusFirst = (e) => {
-  //   if (!e.shiftKey && e.key === "Tab") {
-  //     e.preventDefault();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-  //     const firstItem = menuRef.current.querySelector("a");
-  //     firstItem.focus();
-  //   }
-  // };
   const toggleMenu = () => {
-    if (isMenuShow) {
-      // 닫힘
-      setIsMenuShow(!isMenuShow); // 메뉴 보이기 상태를 토글
-      localStorage.setItem("toc", false);
-    } else {
-      // 열림
-      setIsMenuShow(!isMenuShow); // 메뉴 보이기 상태를 토글
-      localStorage.removeItem("toc", false);
-    }
-    setIsToggled(!isToggled);
+    setIsMenuShow(!isMenuShow);
   };
+
   return (
     <div className={styles.toc}>
       <div className={styles.tocWrap}>
-        {!isMenuShow ? (
-          <>
-            <button onClick={toggleMenu} className={styles.tocBar}>
-              목차
-              <img src={Toggle} alt="" />
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              onClick={toggleMenu}
-              className={`${styles.tocBar} ${isMenuShow ? styles.opened : ""}`}
-            >
-              목차
-              <img src={Toggle} alt="" />
-            </button>
-            <div className={styles.positionWrap}>
-              <Toc toggleMenu={toggleMenu} />
-              <div className={styles.subBanner}>
-                <SubBanner />
-              </div>
+        <button
+          onClick={toggleMenu}
+          className={`${styles.tocBar} ${isMenuShow ? styles.opened : ""}`}
+        >
+          목차
+          <img
+            src={Toggle}
+            alt=""
+            className={isMenuShow ? styles.rotated : ""}
+          />
+        </button>
+        {isMenuShow && (
+          <div className={styles.positionWrap}>
+            <Toc toggleMenu={toggleMenu} />
+            <div className={styles.subBanner}>
+              <SubBanner />
             </div>
-          </>
+          </div>
         )}
       </div>
     </div>
